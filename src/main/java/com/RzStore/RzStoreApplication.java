1c94be67019037375b6b96437f8d475f09840eae
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-10-04 22:48:55 -0300

    Initial commit

---
+++ src/main/java/com/RzStore/RzStoreApplication.java
@@ -0,0 +1,13 @@
+package com.RzStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RzStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RzStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RzStore/RzStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RzStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RzStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
